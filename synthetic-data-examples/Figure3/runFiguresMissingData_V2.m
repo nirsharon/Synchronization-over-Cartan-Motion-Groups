3574a1f8f49a16aee50c206587e87c908a0a200c
Nir Sharon <nsharon@math.princeton.edu>
2017-12-08 16:03:09 -0500

    Update runFiguresMissingData_V2.m

--- synthetic-data-examples/Figure3/runFiguresMissingData_V2.m
+++ synthetic-data-examples/Figure3/runFiguresMissingData_V2.m
@@ -1,6 +1,7 @@
 % script name: "runFiguresMissingData_V2"
-
-% scenarios: missing data, with fixed or vary noise (EIG-based)
+% 
+% This script forms Figure 3 (with enough repeated iterations) 
+% Scenarios: missing data, with fixed or vary noise 
 
 % VERSION December 17, NS
 % main changes: correcting the noise model (Guassian)
@@ -22,7 +23,7 @@ n = n1;
 [err1, snr1, dom1, lambda_vals1] = MakeFigure_MissingDataWfixedNoise_V2(n1, d1);
 
 nameit = ['Missing_n',num2str(n1),'d',num2str(d1),'V2'];
-QuickSaveMissingDataFigure_V1(nameit, [min(dom1), max(dom1)], err1);
+QuickSaveMissingDataFigure_V2(nameit, [min(dom1), max(dom1)], err1);
 runtime1 = toc
 
 
@@ -32,7 +33,7 @@ tic
 
 xrange = [(min(snr3)), max(snr3)];
 nameit = ['Fixed_', num2str(100*AvailData3) ,'_percent_W_Vary_noise_n',num2str(n1),'d',num2str(d1),'V2'];
-QuickSaveMissingDataFigure_V1(nameit, xrange, err3);
+QuickSaveMissingDataFigure_V2(nameit, xrange, err3);
 runtime3 = toc
 
 %
@@ -40,7 +41,7 @@ tic
 [err4, snr4, lambda_vals4] = MakeFigure_MissingDataWvaryNoise_V2(n2, d2);
 xrange = [(min(snr4)), max(snr4)];
 nameit = ['MissingW_varynoise_n',num2str(n2),'d',num2str(d2),'V2'];
-QuickSaveMissingDataFigure_V1(nameit, xrange);
+QuickSaveMissingDataFigure_V2(nameit, xrange);
 runtime4 = toc
 
 save('data_Figure3_v2');
