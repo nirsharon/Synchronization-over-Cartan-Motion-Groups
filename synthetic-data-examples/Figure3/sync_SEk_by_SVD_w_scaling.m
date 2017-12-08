2fd399443d854a4012fc88ca9d9ef44cd511804b
Nir Sharon <nsharon@math.princeton.edu>
2017-12-08 16:04:52 -0500

    Update sync_SEk_by_SVD_w_scaling.m

--- synthetic-data-examples/Figure3/sync_SEk_by_SVD_w_scaling.m
+++ synthetic-data-examples/Figure3/sync_SEk_by_SVD_w_scaling.m
@@ -1,8 +1,12 @@
 function [ estimations ] = sync_SEk_by_SVD_w_scaling(Affinity_mat, W, k, lambda)
-% 
+% This function wrapps "sync_SEk_by_SVD" to allow scaling on the translational parts
+%
+% NS Dec 2017
+
 n = size(W,1);
 new_affinity = zeros(n);
 
+% scaling the input
 for l=1:n
     for j=1:n
         if W(j,l)~=0
@@ -15,15 +19,15 @@ for l=1:n
     end
 end
 
+% run "sync_SEk_by_SVD"
 [ scaled_estimation] = sync_SEk_by_SVD(new_affinity, W, k);
 
-
+% rescaling back the result
 estimations = zeros(k+1,k+1,n);
 for j=1:n
     estimations(:,:,j) = scaled_estimation(:,:,j);
     estimations(1:k,k+1,j) = estimations(1:k,k+1,j)*lambda;
 end
-
 
 end
 
